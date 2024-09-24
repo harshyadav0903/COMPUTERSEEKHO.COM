103a794fbc7fad86148759e28be2defad1a67723
Harsh Yadav <147296826+harshyadav0903@users.noreply.github.com>
2024-09-24 21:55:07 +0530

    Delete Frontend/membersfrontend 11 directory

--- Frontend/membersfrontend 11/src/MoreDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
-export default function MoreDetails() {
-   const id= useParams("id");
-  return (
-    <div>{id}</div>
-  )
-}
